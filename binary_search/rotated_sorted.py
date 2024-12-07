7ba7e79220b594bded242eeb8aa624f50244bb5b
elberth cabrales <elberthcabrales@gmail.com>
2024-12-06 20:24:48 -0600

    Add set explication

--- binary_search/rotated_sorted.py
+++ binary_search/rotated_sorted.py
@@ -5,29 +5,30 @@ Samples
 """
 from typing import List
 
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        left, right = 0, n - 1
-        while left <= right:
-            mid = left + ((right - left) // 2)
+        """
+            if the part in mid is soreted validate normal binary searse else
+        """
+        lo = 0
+        hi = len(nums) - 1
 
-            # Case 1: find target
+        while lo <= hi:
+            mid = (hi + lo) // 2
+
             if nums[mid] == target:
                 return mid
 
-            # Case 2: subarray on mid's left is sorted
-            elif nums[mid] >= nums[left]:
-                if target >= nums[left] and target < nums[mid]:
-                    right = mid - 1
+            # is sorted this part
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target and target < nums[mid]:
+                    hi = mid - 1
                 else:
-                    left = mid + 1
-
-            # Case 3: subarray on mid's right is sorted.
+                    lo = mid + 1
             else:
-                if target <= nums[right] and target > nums[mid]:
-                    left = mid + 1
+                if nums[hi] >= target and target > nums[mid]:
+                    lo = mid + 1
                 else:
-                    right = mid - 1
+                    hi = mid - 1
+
         return -1
